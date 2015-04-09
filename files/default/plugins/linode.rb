1a00aa651880c2d9e73c02105c54f6f6a9f9aac0
Logan Koester <logan@logankoester.com>
2015-04-09 14:43:23 +0000

    Fixes up ohai plugin

--- files/default/plugins/linode.rb
+++ files/default/plugins/linode.rb
@@ -1,6 +1,6 @@
 # Adapted from https://github.com/chef/ohai/blob/master/lib/ohai/plugins/linode.rb
 Ohai.plugin(:Linode) do
-  provides 'linode', 'cloud'
+  provides 'linode'
   depends 'kernel', 'network'
 
   # Checks for matching linode kernel name
@@ -18,18 +18,17 @@ Ohai.plugin(:Linode) do
     prefixlen = '24' if name == :public_ip
     if interface_data = network[:interfaces][interface]
       interface_data[:addresses].each do |key, info|
-        linode[name] = key if info['family'] == 'inet' && info['prefixlen'] == prefixlen
+        return key if (info['family'] == 'inet') && (info['prefixlen'] == prefixlen)
       end
     end
   end
 
-  collect_data do
+  collect_data(:linux) do
     if looks_like_linode?
+      interface = network[:default_interface]
       linode Mash.new
-      interface = network[:interfaces][:default_interface]
-      get_ip_address(:public_ip, interface)
-      get_ip_address(:private_ip, interface)
-      hint?('linode').each{|k,v| linode[k] = v } if hint?('linode').kind_of?( Hash )
+      linode[:private_ip] = get_ip_address(:private_ip, interface)
+      linode[:public_ip] = get_ip_address(:public_ip, interface)
     end
   end
 
