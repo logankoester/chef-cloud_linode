409f7ec2f7ad27e24d7969b4d67127169d33ee07
Logan Koester <logan@logankoester.com>
2015-04-09 16:40:06 +0000

    Override incorrect networking attributes with the correct values.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,15 @@ ohai 'reload cloud_linode' do
   plugin 'cloud_linode'
   action :reload
 end
+
+node.set['linode']['public_ip'] = node['cloud_linode']['public_ip']
+
+node.set['cloud']['public_ips'] = [node['cloud_linode']['public_ip']]
+node.set['cloud']['private_ips'] = [node['cloud_linode']['private_ip']]
+node.set['cloud']['public_ipv4'] = node['cloud_linode']['public_ip']
+node.set['cloud']['local_ipv4'] = node['cloud_linode']['private_ip']
+node.set['cloud']['public_hostname'] = node['fqdn']
+node.set['cloud']['local_hostname'] = node['hostname']
+
+node.set['cloud_v2']['public_ipv4_addrs'] = [node['cloud_linode']['public_ip']]
+node.set['cloud_v2']['public_ipv4'] = node['cloud_linode']['public_ip']
