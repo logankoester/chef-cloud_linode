6836af2f2d715900e5a26c4999fb41c84dd5ed03
Logan Koester <logan@logankoester.com>
2016-11-05 19:16:18 +0000

    Bumps 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/logankoester/chef-cloud_linode/'
 issues_url 'https://github.com/logankoester/chef-cloud_linode/issues'
 depends 'ohai', '>= 4.2.2'
-version '0.1.0'
+version '0.2.0'
